373f3416c71a94a0c6964f85f54ba8819c2b38a0
David James Wyly <david.wyly@gmail.com>
2018-10-13 10:07:00 -0600

    patient controller changes

--- src/Davidwyly/Mb/Http/Controller/PatientController.php
+++ src/Davidwyly/Mb/Http/Controller/PatientController.php
@@ -10,33 +10,78 @@ use Davidwyly\Mb\Exception\ControllerException;
 
 class PatientController extends Controller implements Parse\Json, Parse\Xml
 {
+    /**
+     * Trait to collect JSON from the controller's request
+     *
+     * @see collectJson()
+     */
     use Collect\Json;
+
+    /**
+     * Trait to collect XML from the controller's request
+     *
+     * @see collectXml()
+     */
     use Collect\Xml;
 
+    /**
+     * Create controller action
+     */
     public function create()
     {
         try {
-            if ($this->request->isXml()) {
-                $xml  = $this->collectXml();
-                $data = $this->parseXml($xml);
-            } else if ($this->request->isJson()) {
-                $json = $this->collectJson();
-                $data = $this->parseJson($json);
-            } else {
-                throw new ControllerException("HTTP Content Type '{$this->request->http_content_type}' is not allowed",
-                    self::HTTP_CLIENT_ERROR);
-            }
-
-            $patient = new Patient();
-            $patient->create($data);
-            $this->renderSuccess($data, self::HTTP_CREATED);
+            $request_data = $this->getRequestData();
+            $patient      = new Patient();
+            $patient->create($request_data);
+            $this->renderSuccess($request_data, self::HTTP_CREATED);
         } catch (\Exception $e) {
             $this->renderFail($e);
         }
     }
 
+    /**
+     * @return array
+     * @throws ControllerException
+     */
+    private function getRequestData()
+    {
+        if ($this->request->isXml()) {
+            $xml = $this->collectXml();
+            return $this->parseXml($xml);
+        }
+        if ($this->request->isJson()) {
+            $json = $this->collectJson();
+            return $this->parseJson($json);
+        }
+
+        throw new ControllerException("HTTP Content Type '{$this->request->http_content_type}' is not allowed",
+            self::HTTP_CLIENT_ERROR);
+    }
+
+    /**
+     * Parse\Json interface method
+     *
+     * @param \stdClass $json
+     *
+     * @return array
+     * @throws ControllerException
+     */
     public function parseJson(\stdClass $json): array
     {
+        $required_fields = [
+            'firstName',
+            'lastName',
+            'patientUid',
+            'dateOfBirth',
+        ];
+
+        $missing_fields = [];
+        foreach ($required_fields as $required_field) {
+            if (empty($json->{$required_field})) {
+                $missing_fields[] = $required_field;
+            }
+        }
+
         preg_match('/(?<=patientid)\d+$/', $json->patientUid, $matches);
         if (!isset($matches[0])) {
             throw new ControllerException("External ID '$json->patientUid' does not match expected pattern",
@@ -53,6 +98,17 @@ class PatientController extends Controller implements Parse\Json, Parse\Xml
         ];
     }
 
+    private function validateJson() {
+
+    }
+
+    /**
+     * Parse\Xml interface method
+     *
+     * @param \SimpleXMLElement $xml
+     *
+     * @return array
+     */
     public function parseXml(\SimpleXMLElement $xml): array
     {
         $date_of_birth = (string)$xml->PatientDemographics->DOB;
